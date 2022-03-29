7e0305e6f3f3437d8c940f13f496e5f795e1e2c0
StandinMeme <ianjohn0101@gmail.com>
2022-03-29 09:36:14 +0700

    latest 10:36am sleep sako hehe

--- user/donation.php
+++ user/donation.php
@@ -20,6 +20,10 @@
                         <td><input type='text' name =  'contact_number' required/></td>
                     </tr>
                     <tr>
+                        <td>Proof of Payment: </td>
+                        <td><input type = 'file' name = 'proof_photo' required /></td>
+                    </tr>
+                    <tr>
                         <td>Suffix: </td>
                         <td>
                             <select name = 'suffix'>
