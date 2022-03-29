438e62728247fe89a81efe90a82090a32c0370de
StandinMeme <ianjohn0101@gmail.com>
2022-03-29 09:29:19 +0700

    view Org and Donation user side

--- user/avail_service.php
+++ user/avail_service.php
@@ -1,69 +1,42 @@
-<?php  
-    include("inc/db.php");
-    if(isset($_GET['avail_service']))
+<?php
+    session_start();
+
+    if(!isset($_SESSION['user_username']))
     {
-        $user_username = $_SESSION['user_username'];
+        echo "<script>window.open('login.php', '_self');</script>";
+    }
+    else
+    {
+        if(isset($_GET['avail_service']))
+        {
+            include("inc/db.php");
+            if(isset($_GET['avail_service']))
+            {
+                $service_id = $_GET['avail_service'];
+                $query = $con->prepare("SELECT * FROM services WHERE service_id = '".$service_id."'");
+                $query->setFetchMode(PDO:: FETCH_ASSOC);
+                $query->execute();
 
-        $getUser = $con->prepare("SELECT * FROM users_table WHERE user_username = '$user_username'");
-        $getUser->setFetchMode(PDO:: FETCH_ASSOC);
-        $getUser->execute();
+                $row = $query->fetch();
 
-        $row = $getUser->fetch();
-        $user_id = $row['user_id'];
-
-
-        $service_id = $_GET['avail_service'];
-        $sql = $con->prepare("SELECT * FROM services WHERE service_id = '$service_id'");
-        $sql->setFetchMode(PDO:: FETCH_ASSOC);
-
-        $row_service = $sql->fetch();
-
-        echo 
-        "<form method = 'POST' enctype = 'multipart/form-data'>
-        <tr>
-            <td><input type = 'hidden' name = 'pet_center_id' value = ".$user_id."</td>
-        </tr>
-        <tr>
-            <td>Service Cost: </td>
-            <td><input type = 'hidden' name = 'service_cost' value = ".$row_service['service_cost']."</td>
-            <td>".$row_service['service_cost']."</td>
-        </tr>
-        <tr>
-            <td>Date and Time Reservation: </td>
-            <td><input type ='datetime-local' name = 'reservation_date' /></td>
-        </tr>
-        <tr>
-               <td>Accept Coupons: </td>
-               <td>
-                    <input type='checkbox' id='chkCoupon' onclick='ShowHideDiv(this)' /> Yes
-                </td>  
-                <td id = 'coupon_code' style = 'display:none; margin-left: -200px'>
-                    Coupon Code: <input type ='text' name = 'coupon_code' value = ".generateRandomString()." readonly/><button>Generate Another Code</button>
-                </td>
-        </tr> -->
-        <tr>
-            <input type = 'hidden' name = 'reserve' value = ".$row_service['service_id']." />
-            <a href = 'reserve_service.php'>Reserve</a>
-        </tr>
-        </form>";
+                echo 
+                "<form method = 'POST'>
+                    <tr>
+                        <td>Service Cost: </td>
+                        <td><input type = 'hidden' name = 'service_cost' value = ".$row['service_cost']." </td>
+                        <td>".$row['service_cost']."</td>
+                    </tr><br>
+                    <tr>
+                        <td>Coupon Code: <input type = 'text' name = 'coupon_code'</td>
+                    </tr><br>
+                    <tr>
+                        <td><input type = 'hidden' name = 'reserve' value = ".$row['service_id']."</td>
+                        <td><a href = 'reserve_service'>RESERVE</a></td>
+                    </tr>
+                </form>";
+            }
+        }
     }
 ?>
 
-<?php
-    function generateRandomString($length = 8) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
-    ?>
-
-<script type="text/javascript">
-    function ShowHideDiv(chkCoupon) {
-        var coupon_code = document.getElementById("coupon_code");
-        coupon_code.style.display = chkCoupon.checked ? "block" : "none";
-    }
-</script>
+       
