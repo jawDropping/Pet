35904fa619efd9b71880b6fd6a454cff5ce5f348
jawDropping <carlosjoshua.official@gmail.com>
2022-04-06 12:16:35 +0800

    april 5

--- user/inc/function.php
+++ user/inc/function.php
@@ -5,48 +5,6 @@
     {
         include("inc/db.php");
         
-        echo "<div id ='signUpForm'>
-        <div class='signUpForm'>
-            <h3>Registration</h3>
-                <form method = 'POST' enctype = 'multipart/form-data'>
-                    <table>
-                        <tr>
-                            <td>Name: </td>
-                            <td><input type='text' name = 'user_username' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Password: </td>
-                            <td><input type='text' name =  'user_password' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Email: </td>
-                            <td><input type='text' name =  'user_email' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Municipality: </td>
-                            <td><input type='text' name =  'municipality' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Barangay: </td>
-                            <td><input type='text' name =  'barangay' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Full Address: </td>
-                            <td><input type='text' name =  'address' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Contact Number: </td>
-                            <td><input type='text' name =  'user_contactnumber' required/></td>
-                        </tr>
-                        <tr>
-                            <td>Photo: </td>
-                            <td><input type='file' name =  'user_profilephoto' required/></td>
-                        </tr>
-                    </table>
-                    <button name = 'add_user'>Register</button>
-                </form>
-            </div>
-        </div>";
         
         if(isset($_POST['add_user']))
         {
@@ -63,7 +21,7 @@
         
             move_uploaded_file($user_profilephoto_tmp,"../uploads/user_profile/$user_profilephoto");
 
-            $add_user = $con->prepare("INSERT INTO users_table(
+            $add_user = $con->prepare("INSERT INTO users_table (
                 user_username,
                 user_password,
                 user_email,
--- user/signup.php
+++ user/signup.php
@@ -1,3 +1,45 @@
+<div id ='signUpForm'>
+        <div class='signUpForm'>
+            <h3>Registration</h3>
+                <form method = 'POST' enctype = 'multipart/form-data'>
+                    <table>
+                        <tr>
+                            <td>Name: </td>
+                            <td><input type='text' name = 'user_username' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Password: </td>
+                            <td><input type='text' name =  'user_password' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Email: </td>
+                            <td><input type='text' name =  'user_email' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Municipality: </td>
+                            <td><input type='text' name =  'municipality' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Barangay: </td>
+                            <td><input type='text' name =  'barangay' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Full Address: </td>
+                            <td><input type='text' name =  'address' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Contact Number: </td>
+                            <td><input type='text' name =  'user_contactnumber' required/></td>
+                        </tr>
+                        <tr>
+                            <td>Photo: </td>
+                            <td><input type='file' name =  'user_profilephoto' required/></td>
+                        </tr>
+                    </table>
+                    <button name = 'add_user'>Register</button>
+                </form>
+            </div>
+        </div>
 <?php 
     include("inc/function.php");
     call_user_func('signUp');
