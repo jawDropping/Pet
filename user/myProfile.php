a690f0bbee4dd23bcb9b7fa57e279b322cb7f73e
jawDropping <carlosjoshua.official@gmail.com>
2022-04-20 11:54:25 +0800

    mrProfile

--- user/myProfile.php
+++ user/myProfile.php
@@ -2,6 +2,7 @@
     <head>
     <title>Pet Society</title>
         <link rel = "stylesheet" href="css/style.css" />
+        <link rel = "stylesheet" href="css/profile.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Fredoka+One&family=Open+Sans:wght@500&family=Palette+Mosaic&family=Rubik:wght@500&family=Varela+Round&display=swap" rel="stylesheet">
@@ -31,15 +32,23 @@
         $id = $row['user_id'];
 
         echo 
-        "<form method = 'POST' enctype='multipart/form-data'>
-            <div class='profileTable'>
-            <div class = 'photo'>
-                <img src = '../uploads/user_profile/".$row['user_profilephoto']."' />
-                <input type = 'file' name = 'user_profilephoto' class = 'fileUpload' value = '".$row['user_profilephoto']."' required />
-            </div>
-            <p class='name'>User's Name</p>
+        "
+        <div class='profileTable'>
+        <form method = 'POST' enctype='multipart/form-data'>
+            
             <div class = 'contf'>
+            <div class = 'photo'>
+            <div class = 'piktur'>
+            <img id = 'profs' src = '../uploads/user_profile/".$row['user_profilephoto']."' />
+            </div>
+            <div class = 'intpus'>
+            <p class ='uss'>Change Profile Picture</p>
+            <input type = 'file' name = 'user_profilephoto' class = 'fileUpload' value = '".$row['user_profilephoto']."'/>
+            </div>
+             </div>
+            <div id = 'forBckgnd'>
             <div class='formt'>
+            <div class = 'innerFormt'>
                 <div class='username'>
                     <p class='us'>username </p>
                     <input class='user_name'type = 'text' name =  'user_username' value = '".$row['user_username']."' />
@@ -56,20 +65,38 @@
                     <p class = 'us'>Contact Number: </p>
                     <input  class = 'user_name 'type = 'text' name = 'user_contactnumber' value = '".$row['user_contactnumber']."' />
                 </div>
-                <div class = 'usernameb'>
-                    <button name = 'update_user'>Update Profile</button>
+                <div class = 'username'>
+                    <p class = 'us'>Municipality: </p>
+                    <input  class = 'user_name 'type = 'text' name = 'municipality' value = '".$row['municipality']."' />
                 </div>
-                <div class = 'usernameh'>
-                    <button class = 'back'><a href = 'index.php'>Back to Home</a></button>
+                <div class = 'username'>
+                <p class = 'us'> Barangay: </p>
+                <input  class = 'user_name 'type = 'text' name = 'barangay' value = '".$row['barangay']."' />
+                 </div>
+                <div class = 'username'>
+                <p class = 'us'> Street: </p>
+                <input  class = 'user_name 'type = 'text' name = 'user_address' value = '".$row['user_address']."' />
+                 </div>
+                 <div></div>
+                </div>
+
+                <div class = 'bottomBtn'>
+
+                <div class = 'btn'>
+                    <button id = 'regs2'><a id = 'bckHm' href = 'index.php'>Back to Home</a></button>
+                </div>
+                <div class = 'btn'>
+                    <button name = 'update_user' id = 'regs'>Update Profile</button>
+                </div>
+                
                 </div>
                 </div>
-                <div class='rightSide'>
-                    
                 </div>
                 </div>
             </div>
             
         </form>
+        </div>
         ";
 
         if(isset($_POST['update_user']))
@@ -78,6 +105,9 @@
             $user_password =  $_POST['user_password'];
             $user_contactnumber = $_POST['user_contactnumber'];
             $user_email = $_POST['user_email'];
+            $user_address = $_POST['user_address'];
+            $barangay = $_POST['barangay'];
+            $municipality = $_POST['municipality'];
 
             $user_profilephoto = $_FILES['user_profilephoto']['name'];
             $user_profilephoto_tmp = $_FILES['user_profilephoto']['tmp_name'];
@@ -90,7 +120,11 @@
                 user_password = '$user_password',
                 user_contactnumber = '$user_contactnumber',
                 user_email = '$user_email',
+                user_address = '$user_address',
+                barangay = '$barangay',
+                municipality = '$municipality',
                 user_profilephoto = '$user_profilephoto'
+
             WHERE 
                 user_id = '$id'");
 
@@ -106,136 +140,8 @@
 
 
 </div>
+<?php
+include ("inc/footer.php"); 
+?>
 </body>
-
-<style>
-    *{
-        padding: 0;
-        margin: 0;
-    }
-    #ordPic{
-        height: 30px;
-        margin-right: 10px;
-    }
-    .useless{
-        display: none;
-    }
-    .mainDiv{
-        width: 90%;
-        margin-left: 5%;
-    }
-    #headist{
-        margin-top: 10px;
-     font-size: 22px;
-    }
-    #confLine{
-        font-size: 14px;
-        background: #ffb830;
-        padding: 5px;
-    }
-    
-    .info{
-        margin-top: 10px;
-    }
-    .info{
-        background: white;
-        border-radius: 5px;
-        width: 100%;
-        margin-bottom: 30px;
-        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
-    }
-    .innerInfo{
-        padding: 10px;
-    }
-    
-    #productDet{
-        background: white;
-        width: 100%;
-        border-radius: 5px;
-        border: 1px solid #0080fe;
-    }
-    #innerproductDet{
-        padding-left: 10px;
-        padding-right: 10px;
-        padding-bottom: 30px;
-        padding-top: 20px;
-    }
-    .btnSection{
-        margin-right: 15%;
-        height: 45px;
-        width: 100%;
-        display: flex;
-        justify-content: right;
-        margin-top: 30px;
-    }
-    .place{
-        background: #ffb830;
-        outline: none;
-        border: none;
-        width: 150px;
-        padding: 10px;
-        border-radius: 10px;
-        margin-right: 10px;
-        color: white;
-        font-weight: bold;
-        
-        
-    }
-    #placelink{
-        text-decoration: none;
-        color: white;
-    }
-    .cancelBtn{
-        background: white;
-        outline: none;
-        border: 1px solid  #ffb830;
-        color:  gray;
-        font-weight: bold;
-        padding: 10px;
-        width: 120px;
-        border-radius: 10px;
-       
-    }
-    .cancelBtn a{
-        text-decoration: none;
-        color:  gray;
-        font-weight: bold;
-    }
-    #locatePng{
-        height: 15px;
-    }
-    #locationDiv{
-        
-        margin-top: 10px;
-    }
-    .group{
-        
-        display: grid;
-        grid-template-columns: 10% 50%;
-        gap: 20px;
-        
-
-    }
-    #groupContainer{
-        width: 90%;
-        margin-left: 20px;
-        margin-top: 20px;
-    }
-    .inputed{
-        height: 42px;
-        border: none;
-        border: 1px solid #ddd;
-        margin-bottom: 10px;
-        padding: 4px;
-    }
-    .tag{
-        font-size: 12px;
-        color: gray;
-        text-align: right;
-        padding-top: 15px;
-    }
-    .ttl{
-        width: 60%;
-    }
-</style>
 </html>
