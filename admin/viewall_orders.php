2e1248a7ba93dbdc8b905c8f664bea1d74fc4271
jawDropping <carlosjoshua.official@gmail.com>
2022-05-30 19:27:40 +0800

    may 30

--- admin/viewall_orders.php
+++ admin/viewall_orders.php
@@ -100,7 +100,6 @@ else
             <p class = 'lebsH'>Order Placed</p>
             <p class = 'lebsH'>Total Amount</p>
             <p class = 'lebsH'>Delivery Date</p>
-            <p class = 'lebsH'>Action</p>
         </div>
 
             <?php
